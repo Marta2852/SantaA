d34679d802a02a93da168c520b0a982f487c8087
ipa23.m.dzerve <ipa23.m.dzerve@pjvt.local>
2024-11-19 15:20:48 +0200

    added stuff

---
+++ backend/index.php
@@ -0,0 +1,20 @@
+<?php
+    require "Aircraft.php";
+
+
+    echo "meow";
+
+    function addFour($x){
+        $rezultats = $x + 4;
+        echo "<br> $rezultats";
+    }
+    
+    addFour(5);
+    addFour(8);
+
+    $manaLidmasina = new Aircraft("Airbus", "A220-300", 120, 850);
+    var_dump($manaLidmasina);
+
+    $manaDestination = new Destination("RIX", 56.924, 23.971);
+    var_dump($manaDestination);
+?>
